b0694b606226877d46e06f391d08251cd4b1406b
Taylor Young <tdy721@gmail.com>
2014-04-02 05:26:34 -0500

    Update web_manage.sh

--- web_manage.sh
+++ web_manage.sh
@@ -2,7 +2,10 @@
 
 function start_cs() {
 	cd ~/commandstar/
-	bash forever.sh
+	echo "Installing/updating dependencies..."
+	npm install --production --loglevel error
+	# run the server using the local coffee-script
+	forever start ~/commandstar/node_modules/coffee-script/bin/coffee ~/commandstarlib/commandstar.coffee
 	echo 'started commandstar'
 }
 
