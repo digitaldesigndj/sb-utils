bd8f20f8190fab31fc2acce24e33bc378beb34c8
Taylor Young <tdy721@gmail.com>
2014-04-02 05:30:35 -0500

    steamcmd notes

--- install_steamcmd.sh
+++ install_steamcmd.sh
@@ -3,3 +3,13 @@ mkdir steamcmd
 cd steamcmd
 wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
 tar -xvzf steamcmd_linux.tar.gz
+echo '|   starting steam cmd'
+echo '|   Do this in steamcmd to install Starbound'
+echo '# login <username>
+# <steamid>
+# <password prompt>
+# force_install_dir /root/starbound
+# app_update 211820
+# exit'
+./steamcmd
+
